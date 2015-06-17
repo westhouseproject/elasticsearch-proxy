4e9a8f4dac931b9f5704e8e80b792861f3e328fe
Salehen Shovon Rahman <sal@linux.com>
2015-06-16 18:04:45 -0700

    Initial commit.

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import request from 'request';
+import config from './config.json';
+
+const app = express();
+
+app.get('/data', (req, res) => {
+  request({
+    method: 'GET', url: config.elasticsearchUrl,
+    body: JSON.stringify({
+      aggs: {
+        values: {
+          date_histogram: {
+            field: 'time',
+            interval: '1M',
+            format: 'yyyy-MM-dd'
+          },
+          aggs: {
+            consumption: {
+              avg: { 'field': 'value' }
+            }
+          }
+        }
+      }
+    })
+  }).pipe(res);
+});
+
+app.listen(3000, function () {
+  console.log(this.address());
+});
