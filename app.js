f48e8b78a4f570d35250467481e3505355277a45
Salehen Shovon Rahman <sal@linux.com>
2015-06-16 18:15:29 -0700

    Enabled cors.

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ import config from './config.json';
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/data', (req, res) => {
   request({
     method: 'GET', url: config.elasticsearchUrl,
